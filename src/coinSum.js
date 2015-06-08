ce05474c6a80b1ee834dbd3f69d616393b6da228
jgiang <jasminegiang@MacBook-Pro.local>
2015-06-07 22:48:35 -0700

    coinSum memoized but incorrect computations

--- src/coinSum.js
+++ src/coinSum.js
@@ -24,34 +24,28 @@ makeChange(1) === 1
 makeChange(2) === 2
 */
 
-function memoize(func) {
-	var results = {};
-	return function() {
-			var args = Array.prototype.slice.call(arguments);
-			if (results[args] == undefined) {
-				results[args] = func.apply(args);
-			}
-			return results[args];
-		}
-}
 
 function coinSum(amt){
-	// var memoized = memoize(partition);
+	var results = {};
 
 	function partition(amt, coin){
-		if(coin === 0){
-			return 0;
+		var args = Array.prototype.slice.call(arguments, 0, 2);
+
+		if (coin === 0 || amt < 0){
+			results[args] = 0;
+			// return 0;
+		} else if (amt <= 1){
+			results[args] = 1;
+			// return 1;
+		} else if (results[args] === undefined){
+			results[args] = partition(amt-coin, coin) + partition(amt, nextMaxCoin(amt, coin));
 		}
-		if(amt < 0){
-			return 0;
-		}
-		if(amt <= 1){
-			return 1;
-		}
-		return partition(amt-coin, coin) + partition(amt, maxCoin(amt, coin));
+		return results[args];
+
+		// return partition(amt-coin, coin) + partition(amt, nextMaxCoin(amt, coin));
 	}
 
-	function maxCoin(amt, max){
+	function nextMaxCoin(amt, max){
 		if(amt >= 200 && max > 200){
 			return 200;
 		}
@@ -62,7 +56,7 @@ function coinSum(amt){
 			return 50;
 		}
 		if(amt >= 20 && max > 20){
-			return 200;
+			return 20;
 		}
 		if(amt >= 10 && max > 10){
 			return 10;
@@ -79,6 +73,6 @@ function coinSum(amt){
 		return 0;
 	}
 
-	return partition(amt, maxCoin(amt, 201));
+	return partition(amt, nextMaxCoin(amt, 201));
 }
 
